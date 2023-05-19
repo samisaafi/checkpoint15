c4d01f5697dd82221a4e59e6bcc3a0b8965932ba
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-05-19 19:03:01 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Profile from './profile/Profile';
 import l1 from '../src/photos/sami.jpg';
 function App() {
+    // Function to handle the name
+
   const handleName = (name) => {
     alert(`Profile user name is ${name}`);
   };
+  // Return the JSX representing the structure of the app
 
   return (
     <div className="App">
